c0abff54724ea3c5562b198b9f5de8b57aaefc6d
cmlutkayd <utkuaydogdu@hotmail.com>
2024-10-18 23:08:04 +0300

    S15D1 proje çözümü

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import org.example.mobile.Contact;
+import org.example.mobile.MobilePhone;
+import org.example.models.Grocery;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+
+        MobilePhone mobilePhone = new MobilePhone("12314564541", new ArrayList<>());
+        mobilePhone.addNewContact(new Contact("utku", "13254567468"));
+
 
     }
 }
